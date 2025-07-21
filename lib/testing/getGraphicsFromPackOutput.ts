cd0955e4380c9df8432d0d7d39e4772b4d45a8cd
seveibar <seveibar@gmail.com>
2025-07-21 13:01:40 -0700

    implement get graphics from pack output

--- lib/testing/getGraphicsFromPackOutput.ts
+++ lib/testing/getGraphicsFromPackOutput.ts
@@ -1,8 +1,41 @@
 import { GraphicsObject, Rect } from "graphics-debug"
 import type { PackOutput } from "../types"
+import { getComponentBounds } from "../geometry/getComponentBounds"
 
 export const getGraphicsFromPackOutput = (
-  input: PackOutput,
+  packOutput: PackOutput,
 ): GraphicsObject => {
-  // TODO
+  const rects: Rect[] = []
+
+  for (const component of packOutput.components) {
+    const bounds = getComponentBounds(component)
+    const width = bounds.maxX - bounds.minX
+    const height = bounds.maxY - bounds.minY
+    const rect: Rect = {
+      center: { x: component.center.x, y: component.center.y },
+      width,
+      height,
+      color: "rgba(0,0,0,0.25)",
+    }
+
+    for (const pad of component.pads) {
+      const { absoluteCenter, offset, size, padId, networkId } = pad
+
+      const padRect: Rect = {
+        center: { x: absoluteCenter.x, y: absoluteCenter.y },
+        width: size.x,
+        height: size.y,
+        color: "rgba(255,0,0,0.8)",
+      }
+
+      rects.push(padRect)
+    }
+
+    rects.push(rect)
+  }
+
+  return {
+    coordinateSystem: "cartesian",
+    rects,
+  }
 }
