bee775290d9e20f3464b28779d7b758254c796d1
seveibar <seveibar@gmail.com>
2025-07-22 16:16:05 -0700

    wip pad conversion

--- lib/testing/convertCircuitJsonToPackInput.ts
+++ lib/testing/convertCircuitJsonToPackInput.ts
@@ -16,14 +16,6 @@ export const convertCircuitJsonToPackInput = (
   const pcbComponents = db.pcb_component.list()
 
   for (const pcbComponent of pcbComponents) {
-    const bounds = getBoundsOfPcbElements(
-      circuitJson.filter(
-        (elm) =>
-          "pcb_component_id" in elm &&
-          elm.pcb_component_id === pcbComponent.pcb_component_id,
-      ),
-    )
-
     const pads: InputPad[] = []
 
     const platedHoles = db.pcb_plated_hole.list({
@@ -31,28 +23,66 @@ export const convertCircuitJsonToPackInput = (
     })
 
     for (const platedHole of platedHoles) {
+      const sx =
+        (platedHole as any).rect_pad_width ??
+        (platedHole as any).outer_diameter ??
+        (platedHole as any).hole_diameter ??
+        0
+      const sy =
+        (platedHole as any).rect_pad_height ??
+        (platedHole as any).outer_diameter ??
+        (platedHole as any).hole_diameter ??
+        0
       const pad: InputPad = {
-        // TODO
+        padId: platedHole.pcb_plated_hole_id,
+        networkId:
+          (platedHole as any).source_net_id ??
+          (platedHole as any).pcb_port_id ??
+          "unknown_net",
+        type: "rect",
+        offset: {
+          x: platedHole.x - pcbComponent.center.x,
+          y: platedHole.y - pcbComponent.center.y,
+        },
+        size: { x: sx, y: sy },
       }
+      pads.push(pad)
     }
 
-    const smtPads = db.pcb_smt_pad.list({
+    const smtPads = db.pcb_smtpad.list({
       pcb_component_id: pcbComponent.pcb_component_id,
     })
 
     for (const smtPad of smtPads) {
-      const pad: InputPad = {
-        // TODO
+      if (smtPad.shape === "polygon") {
+        throw new Error("Polygon pads are not supported in pack layout yet")
       }
+      const pad: InputPad = {
+        padId: smtPad.pcb_smtpad_id,
+        networkId:
+          (smtPad as any).source_net_id ??
+          (smtPad as any).pcb_port_id ??
+          "unknown_net",
+        type: "rect",
+        offset: {
+          x: smtPad.x - pcbComponent.center.x,
+          y: smtPad.y - pcbComponent.center.y,
+        },
+        size: {
+          x: (smtPad as any).width ?? 0,
+          y: (smtPad as any).height ?? 0,
+        },
+      }
+      pads.push(pad)
     }
 
     const inputComponent: InputComponent = {
       componentId: pcbComponent.pcb_component_id,
-      pads: [],
+      pads,
     }
 
     packInput.components.push(inputComponent)
   }
 
-  // TODO
+  return packInput
 }
