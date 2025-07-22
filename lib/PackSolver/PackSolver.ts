56b3e018e717fb97643b96852258e0db4ce75987
seveibar <seveibar@gmail.com>
2025-07-22 13:41:40 -0700

    wip

---
+++ lib/PackSolver/PackSolver.ts
@@ -0,0 +1,45 @@
+import { BaseSolver } from "../solver-utils/BaseSolver"
+import type { InputComponent, PackedComponent, PackInput } from "../types"
+
+/**
+ * The pack algorithm performs the following steps:
+ * 1. Sort the components using the packOrderStrategy
+ * 2. Select the next component to pack
+ * 3. If the first component, pack at center (0,0) and go to step 2
+ * 4. Compute the outline of all packed components with a gap of minGap + max(pad.width, pad.height)/2
+ * 5. Find the point along the outline that minimizes the distance of the pad
+ *    centers within a networkId. If no shared pads, pack to the defaultPackDirection
+ * 6. Add the component at the selected point, with it's pad at the position
+ *    minimizing the distance between the pad centers
+ * 7. To determine the component rotation, find the minimum distance between pad
+ *    centers for the remaining pads at each possible rotation (making sure that
+ *    we never pack such that two pads overlap)
+ * 8. Go to step 2 until all components are packed
+ */
+export class PackSolver extends BaseSolver {
+  packInput: PackInput
+
+  unpackedComponentQueue!: InputComponent[]
+  packedComponents!: PackedComponent[]
+
+  constructor(input: PackInput) {
+    super()
+    this.packInput = input
+  }
+
+  override _setup() {
+    const { components, packOrderStrategy } = this.packInput
+
+    this.unpackedComponentQueue = [...components].sort((a, b) => {
+      if (packOrderStrategy === "largest_to_smallest") {
+        return b.pads.length - a.pads.length
+      }
+      return a.pads.length - b.pads.length
+    })
+  }
+
+  override _step() {
+    // If there are
+    // TODO
+  }
+}
--- site/pack/pack01.page.tsx
+++ site/pack/pack01.page.tsx
@@ -1,6 +1,8 @@
 import { InteractiveGraphics } from "graphics-debug/react"
 import type { PackInput, PackOutput } from "../../lib/types"
 import { getGraphicsFromPackOutput } from "../../lib/testing/getGraphicsFromPackOutput"
+import { convertPackOutputToPackInput } from "../../lib/plumbing/convertPackOutputToPackInput"
+import { pack } from "../../lib/pack"
 
 const manualPackOutput: PackOutput = {
   components: [
@@ -27,6 +29,29 @@ const manualPackOutput: PackOutput = {
         },
       ],
     },
+    {
+      componentId: "U2",
+      center: { x: 0, y: 10 },
+      ccwRotationOffset: 0,
+      pads: [
+        {
+          padId: "U2_P1",
+          networkId: "VCC",
+          type: "rect",
+          offset: { x: -5, y: 0 },
+          size: { x: 1, y: 1 },
+          absoluteCenter: { x: -5, y: 10 },
+        },
+        {
+          padId: "U2_P2",
+          networkId: "GND",
+          type: "rect",
+          offset: { x: 5, y: 0 },
+          size: { x: 1, y: 1 },
+          absoluteCenter: { x: 5, y: 10 },
+        },
+      ],
+    },
   ],
   minGap: 5,
   packOrderStrategy: "largest_to_smallest",
@@ -34,10 +59,20 @@ const manualPackOutput: PackOutput = {
   disconnectedPackDirection: "right",
 }
 
+const packInput: PackInput = convertPackOutputToPackInput(manualPackOutput)
+
 export default () => {
+  const packOutput = pack(packInput)
   return (
-    <InteractiveGraphics
-      graphics={getGraphicsFromPackOutput(manualPackOutput)}
-    />
+    <div>
+      <h2>Manual Pack Output</h2>
+      <InteractiveGraphics
+        graphics={getGraphicsFromPackOutput(manualPackOutput)}
+      />
+      <h2>Automatic Pack Output</h2>
+      {packOutput && (
+        <InteractiveGraphics graphics={getGraphicsFromPackOutput(packOutput)} />
+      )}
+    </div>
   )
 }
