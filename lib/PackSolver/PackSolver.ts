c3a1b08f179d71837eed80fa12dbb3455a116fb1
Severin Ibarluzea <seveibar@gmail.com>
2025-08-01 21:19:48 -0700

    add packFirst support (#8)

--- lib/PackSolver/PackSolver.ts
+++ lib/PackSolver/PackSolver.ts
@@ -50,9 +50,28 @@ export class PackSolver extends BaseSolver {
   }
 
   override _setup() {
-    const { components, packOrderStrategy } = this.packInput
+    const { components, packOrderStrategy, packFirst = [] } = this.packInput
+
+    // Create a map for quick lookup of packFirst priorities
+    const packFirstMap = new Map<string, number>()
+    packFirst.forEach((componentId, index) => {
+      packFirstMap.set(componentId, index)
+    })
 
     this.unpackedComponentQueue = [...components].sort((a, b) => {
+      const aPackFirstIndex = packFirstMap.get(a.componentId)
+      const bPackFirstIndex = packFirstMap.get(b.componentId)
+
+      // If both components are in packFirst, sort by their order in packFirst
+      if (aPackFirstIndex !== undefined && bPackFirstIndex !== undefined) {
+        return aPackFirstIndex - bPackFirstIndex
+      }
+
+      // If only one component is in packFirst, it comes first
+      if (aPackFirstIndex !== undefined) return -1
+      if (bPackFirstIndex !== undefined) return 1
+
+      // Neither component is in packFirst, use the regular strategy
       if (packOrderStrategy === "largest_to_smallest") {
         return b.pads.length - a.pads.length
       }
