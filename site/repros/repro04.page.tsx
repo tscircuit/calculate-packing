9f71c6240d5670e7e064a509717c77d6743a68e2
seveibar <seveibar@gmail.com>
2025-08-13 15:05:03 -0700

    reproduction for bad candidates issue

---
+++ site/repros/repro04.page.tsx
@@ -0,0 +1,844 @@
+import type { PackInput } from "lib/types"
+import { PackDebugger } from "../components/PackDebugger"
+
+const packInput: PackInput = {
+  components: [
+    {
+      componentId: "partition_0",
+      pads: [
+        {
+          padId: "partition_0_body",
+          networkId: "partition_0_disconnected",
+          type: "rect",
+          offset: {
+            x: 0,
+            y: 0,
+          },
+          size: {
+            x: 10.325989700000006,
+            y: 8.400000000000004,
+          },
+        },
+        {
+          padId: "0_pin_U3.1",
+          networkId: "V3_3",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: 1.2000000000000015,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.2",
+          networkId: "U3.2_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -1.8000000000000007,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.3",
+          networkId: "U3.3_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -2.000000000000001,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.4",
+          networkId: "U3.4_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -2.200000000000001,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.5",
+          networkId: "U3.5_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -2.4000000000000012,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.6",
+          networkId: "U3.6_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -2.6000000000000014,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.7",
+          networkId: "U3.7_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -2.8000000000000016,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.8",
+          networkId: "U3.8_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -3.0000000000000018,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.9",
+          networkId: "U3.9_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -3.200000000000002,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.11",
+          networkId: "U3.11_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -3.400000000000002,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.12",
+          networkId: "U3.12_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -3.6000000000000023,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.13",
+          networkId: "U3.13_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -3.8000000000000025,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.14",
+          networkId: "U3.14_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -4.000000000000002,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.15",
+          networkId: "U3.15_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: -0.5000000000000009,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.16",
+          networkId: "U3.16_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: -0.7000000000000011,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.17",
+          networkId: "U3.17_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: -0.9000000000000012,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.18",
+          networkId: "U3.18_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: -1.1000000000000014,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.19",
+          networkId: "U3.19_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: -1.3000000000000014,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.20",
+          networkId: "U3.20_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: -1.9000000000000015,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.21",
+          networkId: "U3.21_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: -2.1000000000000014,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.23",
+          networkId: "V1_1",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: 2.8000000000000016,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.24",
+          networkId: "U3.24_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: -2.7000000000000015,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.25",
+          networkId: "U3.25_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: -2.9000000000000012,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.26",
+          networkId: "U3.26_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: -3.1000000000000014,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.27",
+          networkId: "U3.27_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: 0.0999999999999992,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.28",
+          networkId: "U3.28_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: 0.2999999999999994,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.29",
+          networkId: "U3.29_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: 0.49999999999999956,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.30",
+          networkId: "U3.30_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: 0.6999999999999997,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.31",
+          networkId: "U3.31_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: 0.8999999999999995,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.32",
+          networkId: "U3.32_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: 1.0999999999999996,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.34",
+          networkId: "U3.34_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: 1.2999999999999998,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.35",
+          networkId: "U3.35_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: 1.5,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.36",
+          networkId: "U3.36_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: 1.7000000000000002,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.37",
+          networkId: "U3.37_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: 1.9000000000000004,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.38",
+          networkId: "U3.38_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: 2.500000000000001,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.39",
+          networkId: "U3.39_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: 2.700000000000001,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.40",
+          networkId: "U3.40_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: 2.9000000000000012,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.41",
+          networkId: "U3.41_disconnected",
+          type: "rect",
+          offset: {
+            x: 5.562994850000003,
+            y: 3.1000000000000014,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.43",
+          networkId: "U3.43_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -1.6000000000000005,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.44",
+          networkId: "U3.44_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: 2.0000000000000018,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.45",
+          networkId: "U3.45_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: 1.8000000000000016,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.46",
+          networkId: "U3.46_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -1.4000000000000004,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.47",
+          networkId: "U3.47_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -1.2000000000000002,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.48",
+          networkId: "U3.48_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -1,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.51",
+          networkId: "U3.51_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: 3.0000000000000018,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.52",
+          networkId: "U3.52_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: 3.200000000000002,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.53",
+          networkId: "U3.53_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: 3.4000000000000017,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.54",
+          networkId: "U3.54_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: 3.600000000000002,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.55",
+          networkId: "U3.55_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: 3.8000000000000016,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.56",
+          networkId: "U3.56_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: 4.000000000000002,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_U3.57",
+          networkId: "U3.57_disconnected",
+          type: "rect",
+          offset: {
+            x: 1.7629948500000032,
+            y: -0.39999999999999947,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "0_pin_C7.2",
+          networkId: "GND",
+          type: "rect",
+          offset: {
+            x: 0.9843749000000024,
+            y: 1.497580558780955,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+      ],
+      availableRotationDegrees: [0],
+    },
+    {
+      componentId: "partition_1",
+      pads: [
+        {
+          padId: "partition_1_body",
+          networkId: "partition_1_disconnected",
+          type: "rect",
+          offset: {
+            x: 0,
+            y: 0,
+          },
+          size: {
+            x: 0.5291665999999999,
+            y: 1.0583333000000001,
+          },
+        },
+        {
+          padId: "1_pin_C9.1",
+          networkId: "C9.1_disconnected",
+          type: "rect",
+          offset: {
+            x: -0.00027334999999961695,
+            y: 0.5512093,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "1_pin_C9.2",
+          networkId: "GND",
+          type: "rect",
+          offset: {
+            x: 0.00027334999999961695,
+            y: -0.5512093,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+      ],
+      availableRotationDegrees: [0],
+    },
+    {
+      componentId: "partition_2",
+      pads: [
+        {
+          padId: "partition_2_body",
+          networkId: "partition_2_disconnected",
+          type: "rect",
+          offset: {
+            x: 0,
+            y: 0,
+          },
+          size: {
+            x: 0.5291665999999999,
+            y: 1.0583333000000001,
+          },
+        },
+        {
+          padId: "2_pin_C10.1",
+          networkId: "C10.1_disconnected",
+          type: "rect",
+          offset: {
+            x: -0.00027334999999961695,
+            y: 0.5512093,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "2_pin_C10.2",
+          networkId: "GND",
+          type: "rect",
+          offset: {
+            x: 0.00027334999999961695,
+            y: -0.5512093,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+      ],
+      availableRotationDegrees: [0],
+    },
+    {
+      componentId: "partition_3",
+      pads: [
+        {
+          padId: "partition_3_body",
+          networkId: "partition_3_disconnected",
+          type: "rect",
+          offset: {
+            x: 0,
+            y: 0,
+          },
+          size: {
+            x: 0.5291665999999999,
+            y: 1.0583333000000001,
+          },
+        },
+        {
+          padId: "3_pin_C11.1",
+          networkId: "C11.1_disconnected",
+          type: "rect",
+          offset: {
+            x: -0.0002733499999996447,
+            y: 0.5512093,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+        {
+          padId: "3_pin_C11.2",
+          networkId: "GND",
+          type: "rect",
+          offset: {
+            x: 0.0002733499999996447,
+            y: -0.5512093,
+          },
+          size: {
+            x: 0.01,
+            y: 0.01,
+          },
+        },
+      ],
+      availableRotationDegrees: [0],
+    },
+  ],
+  minGap: 2,
+  packOrderStrategy: "largest_to_smallest",
+  packPlacementStrategy: "minimum_sum_squared_distance_to_network",
+}
+
+export default () => {
+  return <PackDebugger initialPackInput={packInput} />
+}
