cd2cb9725c968961f7423d4300779fbe9b8a1ef0
seveibar <seveibar@gmail.com>
2025-08-18 15:05:07 -0700

    bootstrap new solvers

---
+++ lib/SingleComponentPackSolver/SingleComponentPackSolver.ts
@@ -0,0 +1,16 @@
+import type { GraphicsObject } from "graphics-debug"
+import { BaseSolver } from "lib/solver-utils/BaseSolver"
+
+export class SingleComponentPackSolver extends BaseSolver {
+  override _setup() {
+    super._setup()
+  }
+
+  override _step() {
+    super._step()
+  }
+
+  override visualize(): GraphicsObject {
+    return super.visualize()
+  }
+}
