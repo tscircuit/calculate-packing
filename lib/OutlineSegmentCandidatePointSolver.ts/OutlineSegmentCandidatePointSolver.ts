be9f61741fd85bf62ec31a8c7f08a376fea435a4
seveibar <seveibar@gmail.com>
2025-08-18 17:02:11 -0700

    setup for PackSolver2

--- lib/OutlineSegmentCandidatePointSolver.ts/OutlineSegmentCandidatePointSolver.ts
+++ lib/OutlineSegmentCandidatePointSolver.ts/OutlineSegmentCandidatePointSolver.ts
@@ -1,16 +1,48 @@
+import type { Point } from "@tscircuit/math-utils"
 import type { GraphicsObject } from "graphics-debug"
 import { BaseSolver } from "lib/solver-utils/BaseSolver"
+import type { InputComponent, PackedComponent } from "lib/types"
 
+/**
+ * Given a single segment on the outline, the component's rotation, compute the
+ * optimal position for the rotated component (the position that minimizes the
+ * packStrategy, generally minimizing the sum of the distances to other pads in
+ * the network)
+ */
 export class OutlineSegmentCandidatePointSolver extends BaseSolver {
-  override _setup() {
-    super._setup()
+  outlineSegment: [Point, Point]
+  componentRotationDegrees: number
+  packStrategy:
+    | "minimum_sum_squared_distance_to_network"
+    | "minimum_sum_distance_to_network"
+  minGap: number
+  packedComponents: PackedComponent[]
+  componentToPack: InputComponent
+
+  constructor(params: {
+    outlineSegment: [Point, Point]
+    componentRotationDegrees: number
+    packStrategy:
+      | "minimum_sum_squared_distance_to_network"
+      | "minimum_sum_distance_to_network"
+    minGap: number
+    packedComponents: PackedComponent[]
+    componentToPack: InputComponent
+  }) {
+    super()
+    this.outlineSegment = params.outlineSegment
+    this.componentRotationDegrees = params.componentRotationDegrees
+    this.packStrategy = params.packStrategy
+    this.minGap = params.minGap
+    this.packedComponents = params.packedComponents
+    this.componentToPack = params.componentToPack
   }
 
   override _step() {
-    super._step()
+    // TODO
   }
 
   override visualize(): GraphicsObject {
-    return super.visualize()
+    throw new Error("Not implemented")
   }
 }
--- lib/PackSolver2/PackSolver2.ts
+++ lib/PackSolver2/PackSolver2.ts
@@ -1,13 +1,79 @@
 import type { GraphicsObject } from "graphics-debug"
+import { setPackedComponentPadCenters } from "lib/PackSolver/setPackedComponentPadCenters"
+import { sortComponentQueue } from "lib/PackSolver/sortComponentQueue"
+import { SingleComponentPackSolver } from "lib/SingleComponentPackSolver/SingleComponentPackSolver"
 import { BaseSolver } from "lib/solver-utils/BaseSolver"
+import type { InputComponent, PackedComponent, PackInput } from "lib/types"
 
 export class PackSolver2 extends BaseSolver {
+  declare activeSubSolver: SingleComponentPackSolver | null | undefined
+
+  packInput: PackInput
+
+  unpackedComponentQueue: InputComponent[] = []
+  packedComponents: PackedComponent[] = []
+  componentToPack?: InputComponent | null | undefined
+
+  constructor(packInput: PackInput) {
+    super()
+    this.packInput = packInput
+  }
+
   override _setup() {
-    super._setup()
+    const { components, packOrderStrategy, packFirst = [] } = this.packInput
+
+    this.unpackedComponentQueue = sortComponentQueue({
+      components,
+      packOrderStrategy,
+      packFirst,
+    })
+    this.packedComponents = []
+    this.packFirstComponent()
+  }
+
+  private packFirstComponent(): void {
+    const firstComponentToPack = this.unpackedComponentQueue.shift()!
+
+    const newPackedComponent: PackedComponent = {
+      ...firstComponentToPack,
+      center: { x: 0, y: 0 },
+      ccwRotationOffset: 0,
+      pads: firstComponentToPack.pads.map((p) => ({
+        ...p,
+        absoluteCenter: { x: 0, y: 0 },
+      })),
+    }
+
+    setPackedComponentPadCenters(newPackedComponent)
+    this.packedComponents.push(newPackedComponent)
   }
 
   override _step() {
-    super._step()
+    if (this.solved || this.failed) return
+
+    if (this.unpackedComponentQueue.length === 0) {
+      this.solved = true
+      return
+    }
+
+    if (!this.componentToPack || !this.activeSubSolver) {
+      this.componentToPack = this.unpackedComponentQueue.shift()
+      this.activeSubSolver = new SingleComponentPackSolver({
+        packedComponents: this.packedComponents,
+        componentToPack: this.componentToPack,
+      })
+    }
+
+    this.activeSubSolver._step()
+
+    if (this.activeSubSolver.failed) {
+      this.failed = true
+      return
+    }
+
+    if (this.activeSubSolver.solved) {
+      this.packedComponents.push(this.activeSubSolver.packedComponent)
+    }
   }
 
   override visualize(): GraphicsObject {
