0504c25dee97f91ce26059acde779fa80314aec3
seveibar <seveibar@gmail.com>
2026-08-18 15:28:56 -0700

    refactor: use getSolverName for stable solver name

--- tests/solver-name.test.ts
+++ tests/solver-name.test.ts
@@ -2,5 +2,7 @@ import { expect, test } from "bun:test"
 import { PackSolver2 } from "../lib"
 
 test("PackSolver2 has a stable solver name", () => {
-  expect(PackSolver2.solverName).toBe("PackSolver2")
+  const solver = Object.create(PackSolver2.prototype) as PackSolver2
+
+  expect(solver.getSolverName()).toBe("PackSolver2")
 })
